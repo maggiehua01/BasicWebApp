877acd41c6c78ee1b4ca98ec9d0b32b98b6250bc
Maggie Huang <maggiehua01@gmail.com>
2022-09-19 19:05:53 -0400

    Change welcome message again

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Hey there!</h1>" +
+                "<h1>Hey there Windows lover!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
